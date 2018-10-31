3044de287bd3bb49aef31efa5fda364835d90dc3
Theresa Vocke <tv019@hdm-stuttgart.de>
2018-10-31 12:15:16 +0100

    BO Klasse Pinboard anlegen

--- src/de/hdm/pinit/shared/bo/Pinboard.java
+++ src/de/hdm/pinit/shared/bo/Pinboard.java
@@ -3,24 +3,29 @@ package de.hdm.pinit.shared.bo;
 public class Pinboard extends BusinessObject {
 
 	/**
-	 * 
+	 * <code>SerialVersionUID</code> ist eine Zahl vom Typ long, die zur
+	 * eindeutigen Identifikation der Version einer serialisierbaren Klasse
+	 * dient. Die Versionsnummer wird bei der Serialisierung automatisch jeder
+	 * Klasse hinzugefügt, die das Interface Serializable implementiert
 	 */
 	private static final long serialVersionUID = 1L;
-		
 
 	/**
-	 * Owner der Pinwand
+	 * Jede Pinnwand hat einen Owner, der über die <code>ownerId</code>
+	 * identifiziert werden kann.
+	 */
+
+	private int ownerId;
+
+	/**
+	 * Getter- und Setter-Methoden
 	 */
 		
-	private User user;
-	
-
-	public User getUser() {
-		return user;
+	public int getOwnerId() {
+		return ownerId;
 	}
 
-
-	public void setUser(User user) {
-		this.user = user;
+	public void setOwnerId(int ownerid) {
+		this.ownerId = ownerId;
 	}
 }
