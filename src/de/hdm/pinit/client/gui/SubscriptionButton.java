d3adaaa585f52f96daeae222f888ba3ebbeb04cb
Theresa Vocke <tv019@hdm-stuttgart.de>
2018-11-07 18:40:05 +0100

    Gui anpassungen

--- src/de/hdm/pinit/client/gui/SubscriptionButton.java
+++ src/de/hdm/pinit/client/gui/SubscriptionButton.java
@@ -5,15 +5,15 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SubscriptionButton extends VerticalPanel {
 
-	private VerticalPanel vpSub = new VerticalPanel();
+	//private VerticalPanel vpSub = new VerticalPanel();
 
 	Button subBtn = new Button("Abonnement suchen");
 
 	public void onLoad() {
 		super.onLoad();
 		this.add(subBtn);
+		subBtn.setEnabled(true);
 
-		vpSub.add(subBtn);
 	}
-
+	
 }
